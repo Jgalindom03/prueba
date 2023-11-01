5ca71dff2de4ff07b84d1cbb6b356c4e4149150a
jgalindom2003@gmail.com <jgalindom2003@gmail.com>
2023-11-01 12:46:05 +0100

    updated

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import express from "npm:express@4.18.2";
+import express,{Request, Response} from "npm:express@4.18.2";
 import mongoose from "npm:mongoose@7.6.3";
 
 import postMascota from "./resolvers/postMascota.ts";
@@ -17,8 +17,15 @@ await mongoose.connect(MONGO_URL);
 
 const app = express();
 app.use(express.json());
+app.get("/api/mascotas", async(_req:Request, res:Response) => { //Get de todas las mascotas
+  try{
+    const mascotas = await getMascota;
+    res.json(mascotas);
+  }catch(error){
+    res.json({error:error.message});
+  }
+})
 app.post("/postMascota", postMascota)
-app.get("/getMascota/:nombre", getMascota)
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
